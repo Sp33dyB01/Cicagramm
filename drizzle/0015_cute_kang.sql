fce1996b8f967157a8db8c21b15b5e409fc6808d
Mészáros Gergő <gergnoscope@gmail.com>
2026-02-12 11:30:56 +0100

    elméleti rendezés távolság  vagy név szerint

---
+++ drizzle/0015_cute_kang.sql
@@ -0,0 +1,23 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_felhasznalo` (
+	`fel_id` text PRIMARY KEY NOT NULL,
+	`p_kep` text NOT NULL,
+	`Email` text(50) NOT NULL,
+	`emailVerified` integer NOT NULL,
+	`admin` integer DEFAULT 0,
+	`nev` text NOT NULL,
+	`r_bemutat` text,
+	`irsz` integer,
+	`varos` text,
+	`utca` text(50),
+	`hazszam` integer,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL,
+	`szk` real DEFAULT 1 NOT NULL,
+	`hk` real DEFAULT 1 NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_felhasznalo`("fel_id", "p_kep", "Email", "emailVerified", "admin", "nev", "r_bemutat", "irsz", "varos", "utca", "hazszam", "createdAt", "updatedAt", "szk", "hk") SELECT "fel_id", "p_kep", "Email", "emailVerified", "admin", "nev", "r_bemutat", "irsz", "varos", "utca", "hazszam", "createdAt", "updatedAt", "szk", "hk" FROM `felhasznalo`;--> statement-breakpoint
+DROP TABLE `felhasznalo`;--> statement-breakpoint
+ALTER TABLE `__new_felhasznalo` RENAME TO `felhasznalo`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
